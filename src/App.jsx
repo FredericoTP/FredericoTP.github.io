885de06a959f5fae8a86b840cb27cfde2e376219
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2023-02-18 13:40:10 -0300

    Feat: create page About

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import Introduction from "./pages/Introduction";
 import Header from "./components/Header";
+import Introduction from "./pages/Introduction";
+import About from "./pages/About";
 import './style/App.css';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div>
       <Header />
       <Introduction />
+      <About />
     </div>
   );
 }
---
+++ src/pages/About.jsx
@@ -0,0 +1,9 @@
+function About() {
+  return (
+    <div>
+      <h2>About me</h2>
+    </div>
+  )
+}
+
+export default About;
