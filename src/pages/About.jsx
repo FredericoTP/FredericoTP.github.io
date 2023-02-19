ddb6633d50e17d11b867d8411d528bf924e62cdc
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2023-02-19 19:13:33 -0300

    Feat: Finalização css About

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -32,6 +32,7 @@ function About() {
           <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" alt="github" />
           </a>
         </div>
+        <h3 className="about-subtitle">Stacks</h3>
         <div className="about-stacks-container">
           <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain.svg" alt="html" />
           <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain.svg" alt="css" />
