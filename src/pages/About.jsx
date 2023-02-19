dd7f77cb1944e88839774a52d403c7859686da74
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2023-02-19 12:22:05 -0300

    Feat: Adiciona css básico de About

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,21 +3,32 @@ import '../style/About.css';
 function About() {
   return (
     <div className="about-container">
-      <h2>About me</h2>
-      <div>
+      <h2 className="about-title">About me</h2>
+      <div className="about-text-image-container">
         <div>
-          <img src="https://github.com/FredericoTP/FredericoTP.github.io/blob/main/images/fred.jpeg?raw=true" alt="Fred" />
+          <img
+            className="about-image-profile"
+            src="https://github.com/FredericoTP/FredericoTP.github.io/blob/main/images/fred.jpeg?raw=true" 
+            alt="Fred" />
         </div>
-        <div>
+        <div className="about-text">
           <p>
-            Meu nome é Frederico Tavares Pessoa e moro no interior de Minas Gerais, Brasil. Algumas de minhas paixões são programação, tecnologia, fotografia, astronomia e jogos. No momento estou migrando de área como estudante de Desenvolvimento Web Full Stack. Com o conhecimento que estou adquirindo, espero contribuir para tornar o futuro mais prático e contribuir positivamente na vida das pessoas.
+            Meu nome é Frederico Tavares Pessoa e moro no interior de Minas Gerais, Brasil. Algumas de minhas paixões são programação, tecnologia, fotografia, astronomia e jogos. No momento estou migrando de área como estudante de Desenvolvimento Web Full Stack.
+          </p>
+          <br />
+          <p>
+            Com o conhecimento que estou adquirindo, espero contribuir para tornar o futuro mais prático e contribuir positivamente na vida das pessoas.
           </p>
         </div>
       </div>
-      <div>
-        <div>
-          <a href="blabla">Linkdin</a>
-          <a href="blabla">Github</a>
+      <div className="about-links-stacks-container">
+        <div className="about-links-container">
+          <a href="blabla">
+          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-plain.svg" alt="linkedin"/>
+          </a>
+          <a href="blabla">
+          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" alt="github" />
+          </a>
         </div>
         <div className="about-stacks-container">
           <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain.svg" alt="html" />
