ffdbe3f90f07b7c384860823d3888bea3a7c58e0
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2023-02-19 11:17:41 -0300

    Feat: Adiciona bruto de About

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,6 +4,16 @@ function About() {
   return (
     <div className="about-container">
       <h2>About me</h2>
+      <div>
+        <div>
+          <img src="https://github.com/FredericoTP/FredericoTP.github.io/blob/main/images/fred.jpeg?raw=true" alt="Fred" />
+        </div>
+        <div>
+          <p>
+            Meu nome é Frederico Tavares Pessoa e moro no interior de Minas Gerais, Brasil. Algumas de minhas paixões são programação, tecnologia, fotografia, astronomia e jogos. No momento estou migrando de área como estudante de Desenvolvimento Web Full Stack. Com o conhecimento que estou adquirindo, espero contribuir para tornar o futuro mais prático e contribuir positivamente na vida das pessoas.
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
